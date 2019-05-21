ce9492f8e768a8fba82c94f2b293b2b92df924d4
Uliana Muratova <udmuratova@yandex.ru>
2019-05-21 21:14:01 +0300

    Необходимые статичные элементы + вывод текста на экран

--- app/src/main/java/com/example/ulyaf/cheshirbot/MainActivity.java
+++ app/src/main/java/com/example/ulyaf/cheshirbot/MainActivity.java
@@ -2,12 +2,25 @@ package com.example.ulyaf.cheshirbot;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onButtonClick(View view) {
+        //Почему final
+         final EditText editText = (EditText)findViewById(R.id.editText1);
+         TextView textView = (TextView)findViewById(R.id.textView1);
+
+        String phrase = editText.getText().toString();
+        textView.setText(phrase);
     }
 }
